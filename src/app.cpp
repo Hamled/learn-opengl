076f177b7d97faf8f70b56a807554590dd475b2e
Charles Ellis <hamled@hamled.name>
2016-12-13 11:03:37 -0800

    Use GLFW window scope wrapper function

--- src/app.cpp
+++ src/app.cpp
@@ -9,57 +9,35 @@ static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int act
 int main() {
   glfwSetErrorCallback(glfw_error_callback);
 
-  // Initialize GLFW
-  if(!glfwInit()) {
-    std::cout << "Unable to initialize GLFW!" << std::endl;
-    std::exit(EXIT_FAILURE);
-  }
-  std::cout << "GLFW initialized successfully." << std::endl;
+  const auto res = with_window([](GLFWwindow* window) -> int {
+    // Use the window's OpenGL context
+    glfwMakeContextCurrent(window);
 
-  // Configure our desired context
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    // Load all Open GL functionality w/ glad
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+      std::cout << "Unable to create OpenGL context!" << std::endl;
+      return EXIT_FAILURE;
+    }
 
-  // Create the window
-  const auto window = glfwCreateWindow(640, 480, "Learn OpenGL", nullptr, nullptr);
-  if(window == nullptr) {
-    std::cout << "Unable to create OpenGL context!" << std::endl;
-    glfwTerminate();
-    std::exit(EXIT_FAILURE);
-  }
+    // Listen for key events
+    glfwSetKeyCallback(window, glfw_key_callback);
 
-  // Use the window's OpenGL context
-  glfwMakeContextCurrent(window);
+    // Setup the viewport
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
 
-  // Load all Open GL functionality w/ glad
-  if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-    std::cout << "Unable to create OpenGL context!" << std::endl;
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    std::exit(EXIT_FAILURE);
-  }
+    // Loop until the window should close
+    while(!glfwWindowShouldClose(window)) {
+      // We must wait for events or this
+      // spinloop will prevent anything from happening
+      glfwWaitEvents();
+    }
 
-  // Listen for key events
-  glfwSetKeyCallback(window, glfw_key_callback);
+    return EXIT_SUCCESS;
+  });
 
-  // Setup the viewport
-  int width, height;
-  glfwGetFramebufferSize(window, &width, &height);
-  glViewport(0, 0, width, height);
-
-  // Loop until the window should close
-  while(!glfwWindowShouldClose(window)) {
-    // We must wait for events or this
-    // spinloop will prevent anything from happening
-    glfwWaitEvents();
-  }
-
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  std::exit(EXIT_SUCCESS);
+  std::exit(res);
 }
 
 void glfw_error_callback(int error, const char* description) {
