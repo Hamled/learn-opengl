75472b71eeb1ebac9d02d0ac0bddd8322c321610
Charles Ellis <hamled@hamled.name>
2016-12-12 22:35:06 -0800

    Callback handlers have internal linkage

--- src/app.cpp
+++ src/app.cpp
@@ -2,8 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void glfw_error_callback(int error, const char* description);
-void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+static void glfw_error_callback(int error, const char* description);
+static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main() {
   glfwSetErrorCallback(glfw_error_callback);
