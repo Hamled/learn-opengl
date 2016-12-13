395c8c93429715d375171771179604cba8dddd3f
Charles Ellis <hamled@hamled.name>
2016-12-13 11:02:52 -0800

    Wrapper function for running code in scope of GLFW window

--- src/app.cpp
+++ src/app.cpp
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static int with_window(std::function<  int(GLFWwindow*) > block);
 static void glfw_error_callback(int error, const char* description);
 static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
@@ -69,4 +70,37 @@ void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, in
   if(action == GLFW_PRESS && key == GLFW_KEY_ESCAPE) {
     glfwSetWindowShouldClose(window, GLFW_TRUE);
   }
+}
+
+int with_window(std::function< int(GLFWwindow*) > block) {
+  // Initialize GLFW
+  if(!glfwInit()) {
+    std::cout << "Unable to initialize GLFW!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << "GLFW initialized successfully." << std::endl;
+
+  // Configure our desired context
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  // Create the window
+  const auto window = glfwCreateWindow(640, 480, "Learn OpenGL", nullptr, nullptr);
+  if(window == nullptr) {
+    std::cout << "Unable to create OpenGL context!" << std::endl;
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  // Run our code with the window
+  const auto res = block(window);
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  std::cout << "GLFW terminated successfully." << std::endl;
+
+  return res;
 }
