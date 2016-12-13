c87ce88cb734c47abfb10bb73c536386dea54aa1
Charles Ellis <hamled@hamled.name>
2016-12-12 21:55:00 -0800

    Some comments

--- src/app.cpp
+++ src/app.cpp
@@ -7,17 +7,20 @@ void glfw_error_callback(int error, const char* description);
 int main() {
   glfwSetErrorCallback(glfw_error_callback);
 
+  // Initialize GLFW
   if(!glfwInit()) {
     std::cout << "Unable to initialize GLFW!" << std::endl;
     return 1;
   }
   std::cout << "GLFW initialized successfully." << std::endl;
 
+  // Configure our desired context
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // Create the window
   GLFWwindow* window = glfwCreateWindow(640, 480, "Learn OpenGL", NULL, NULL);
   if(!window) {
     std::cout << "Unable to create OpenGL context!" << std::endl;
@@ -25,8 +28,10 @@ int main() {
     return 1;
   }
 
+  // Use the window's OpenGL context
   glfwMakeContextCurrent(window);
 
+  // Load all Open GL functionality w/ glad
   if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
     std::cout << "Unable to create OpenGL context!" << std::endl;
     glfwDestroyWindow(window);
