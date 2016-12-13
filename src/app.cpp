0bde0cae9f0f00536bce87d8aa0d7dc2cf90b28c
Charles Ellis <hamled@hamled.name>
2016-12-12 21:35:19 -0800

    Setup GLFW error callback

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+void glfw_error_callback(int error, const char* description);
+
 int main() {
+  glfwSetErrorCallback(glfw_error_callback);
+
   if(!glfwInit()) {
     std::cout << "Unable to initialize GLFW!" << std::endl;
     return 1;
@@ -10,4 +14,8 @@ int main() {
   std::cout << "GLFW initialized successfully." << std::endl;
   glfwTerminate();
   return 0;
+}
+
+void glfw_error_callback(int error, const char* description) {
+  std::cout << "GLFW Error " << error << ": " << description << std::endl;
 }
