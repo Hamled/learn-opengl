93f6e89f539b653f3b945eda55bcee94f3a462f4
Charles Ellis <hamled@hamled.name>
2016-12-14 22:29:39 -0800

    Configure vertex buffer

--- src/app.cpp
+++ src/app.cpp
@@ -57,6 +57,10 @@ extern "C" int main() {
         };
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+        //     Then configure that buffer
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
+
         //     Next setup the shader program
         glUseProgram(shaderProg);
 
