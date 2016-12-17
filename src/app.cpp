93d9ae560cf2764323ed1aa5386f895a7e67fdef
Charles Ellis <hamled@hamled.name>
2016-12-16 21:44:12 -0800

    Use indexed drawing

--- src/app.cpp
+++ src/app.cpp
@@ -35,9 +35,11 @@ extern "C" int main() {
       glDeleteShader(fragShader);
 
       // Create and configure our vertex buffer and array objects
-      GLuint VAO = 0, VBO = 0;
+      GLuint VAO = 0, VBO = 0, EBO = 0;
       glGenVertexArrays(1, &VAO);
       glGenBuffers(1, &VBO);
+      glGenBuffers(1, &EBO);
+
       glBindVertexArray(VAO);
         // First fill the vertex buffer
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -49,7 +51,15 @@ extern "C" int main() {
         };
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        // Then configure that buffer
+        // And setup our element indices
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+        const GLuint indices[] = {
+          0, 1, 2 // First triangle
+        };
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        // Then configure the vertex attributes
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
         glEnableVertexAttribArray(0);
       glBindVertexArray(NULL);
@@ -73,7 +83,7 @@ extern "C" int main() {
         glBindVertexArray(VAO);
 
         //     Actually render
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         //     Unbind the vertex array
         glBindVertexArray(NULL);
