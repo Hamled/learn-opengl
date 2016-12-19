4b5603ed26bb60e12df9237bdacaaefde17a0dab
Charles Ellis <hamled@hamled.name>
2016-12-18 22:17:56 -0800

    Setup texture coords vertex attribute

--- src/app.cpp
+++ src/app.cpp
@@ -51,11 +51,11 @@ extern "C" int main() {
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
         const GLfloat vertices[] = {
-          // Position        // Color delta
-          -0.5f, -0.5f, 0.f,  0.2f, -0.1f, 1.f,
-           0.5f, -0.5f, 0.f, -0.1f,  0.2f, 2.f,
-          -0.5f,  0.5f, 0.f,  0.1f,  0.2f, 2.f,
-           0.5f,  0.5f, 0.f, -0.2f, -0.1f, 1.f
+          // Position        // Color delta     // Texture coords
+          -0.5f, -0.5f, 0.f,  0.2f, -0.1f, 1.f, 0.f, 0.f,
+           0.5f, -0.5f, 0.f, -0.1f,  0.2f, 2.f, 1.f, 0.f,
+          -0.5f,  0.5f, 0.f,  0.1f,  0.2f, 2.f, 0.f, 1.f,
+           0.5f,  0.5f, 0.f, -0.2f, -0.1f, 1.f, 1.f, 1.f
         };
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -68,10 +68,12 @@ extern "C" int main() {
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
         // Then configure the vertex attributes
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
         glEnableVertexAttribArray(0);
         glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
       glBindVertexArray(NULL);
 
       // Load texture from JPG file
